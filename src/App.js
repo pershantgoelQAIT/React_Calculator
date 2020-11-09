bd66a1ec88ccdddd0e23aa14abd759b671793590
pershantgoelQAIT <pershantgoel@qainfotech.com>
2020-11-09 18:24:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Keypad from './components/keypad';
+import Result from './components/result';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+      super();
+
+      this.state = {
+          result: ""
+      }
+  }
+
+  onClick = button => {
+
+    if(button === "="){
+        this.calculate();
+    }
+
+    else if(button === "C"){
+        this.reset();
+    }
+
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+};
+
+
+calculate = () => {
+    try {
+        this.setState({
+            // eslint-disable-next-line
+            result: (eval(this.state.result) || "" ) + ""
+        })
+    } catch (e) {
+        this.setState({
+            result: "error"
+        })
+
+    }
+};
+
+reset = () => {
+    this.setState({
+        result: ""
+    })
+};
+
+backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+};
+
+  render() {
+      return (
+          <div>
+              <div className="calculator-body">
+                  <h1>Simple Calculator</h1>
+                  <Result result={this.state.result}/>
+                  <Keypad onClick={this.onClick}/>
+              </div>
+          </div>
+      );
+  }
 }
 
 export default App;
